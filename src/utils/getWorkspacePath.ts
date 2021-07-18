73d055cff09dccd189d57b373fde0de85824d887
fengshiyu <fengshiyu@kuaishou.com>
2021-07-18 11:13:32 +0800

    update: test

--- src/utils/getWorkspacePath.ts
+++ src/utils/getWorkspacePath.ts
@@ -2,10 +2,8 @@ import * as vscode from 'vscode';
 
 export default (): string => {
     const curFilePath = vscode.window.activeTextEditor?.document.fileName;
-    const key = (
-        vscode.workspace.workspaceFolders as vscode.WorkspaceFolder[]
-    ).filter((folder: vscode.WorkspaceFolder) =>
+    const key = vscode.workspace.workspaceFolders?.filter((folder: vscode.WorkspaceFolder) =>
         curFilePath?.includes(folder.uri.path)
     )[0].uri.path;
-    return key;
+    return key || 'ac-default-key';
 };
